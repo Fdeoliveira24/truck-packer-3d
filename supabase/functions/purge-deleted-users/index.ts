5ad55a9b2f5672b8453349187c622c4ebc83db67
Fdeoliveira24 <francisco.puff@gmail.com>
2026-05-07 22:06:10 -0400

    fix(account): clarify retired purge endpoint copy

--- supabase/functions/purge-deleted-users/index.ts
+++ supabase/functions/purge-deleted-users/index.ts
@@ -1,7 +1,7 @@
 import { getAllowedOrigin, json } from "../_shared/cors.ts";
 
 const RETIRED_ERROR =
-  "This endpoint has been retired. Account purge is managed by the scheduled purge-deleted-accounts function.";
+  "This endpoint has been retired. Account purge is not available through this legacy endpoint.";
 
 Deno.serve(async (req) => {
   const origin = getAllowedOrigin(req);
